5a8caff6b162ae99eaff5090bbc1c9ff01c0f5c8
Steven Barragán <me@steven.mx>
2016-11-29 14:43:28 -0800

    Refactor optimizer

--- lib/refile/optimizer.rb
+++ lib/refile/optimizer.rb
@@ -1,19 +1,24 @@
 require "refile/optimizer/version"
+require "refile/mini_magick"
+require "image_optim"
+require "image_optim_pack"
 
 module Refile
   module Optimizer
-    %w{fill fit limit pad convert resample}.each do |name|
-      define_method name do |*args|
-        file = super(*args)
+    class Processor < Refile::MiniMagick
+      %w{fill fit limit pad convert resample}.each do |name|
+        define_method name do |*args|
+          file = super(*args)
 
-        ImageOptim.optimize_image!(file.path)
+          ImageOptim.optimize_image!(file.path)
 
-        file
+          file
+        end
       end
     end
   end
 end
 
 %w{filloptim fitoptim limitoptim padoptim convertoptim resampleoptim}.each do |name|
-  Refile.processor(name.to_sym, Refile::Optimizer.new(name.gsub(/optim/, "")))
+  Refile.processor(name.to_sym, Refile::Optimizer::Processor.new(name.gsub(/optim/, "")))
 end
